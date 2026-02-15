59c324a922ece158f1d64237b1575701e2142c16
Davide Siracusano <152174921+DavideSiracusano@users.noreply.github.com>
2026-02-15 19:18:32 +0100

    fix: exclude backend from tsconfig and webpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      ignored: /backend/,
+    };
+    return config;
+  },
+};
 
 export default nextConfig;
