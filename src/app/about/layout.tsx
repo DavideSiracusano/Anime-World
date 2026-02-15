2a630f048e921f08dff7cfc985f4424c6cb218bd
Davide Siracusano <152174921+DavideSiracusano@users.noreply.github.com>
2026-02-15 19:15:22 +0100

    fix: sezione about

--- src/app/about/layout.tsx
+++ src/app/about/layout.tsx
@@ -3,7 +3,7 @@ import { ReactNode } from "react";
 export const metadata = {
   title: "Chi Siamo - My AnimeWorld",
   description:
-    "My AnimeWorld è la piattaforma definitiva per gli appassionati di anime. Ricerca, scopri e salva i tuoi anime preferiti con le API di Trace.moe e Jikan.",
+    "My AnimeWorld è la piattaforma ideale per gli appassionati di anime. Ricerca, scopri e salva i tuoi anime preferiti con le API di Trace.moe e Jikan.",
 };
 
 interface AboutLayoutProps {
